77783b7a8ce8b7014b5449c3217f5128ebd446e4
pemiinem <190139323@aluno.unb.br>
2022-09-06 16:24:36 -0300

    update firebase with auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import logoIcb from '../assets/images/logo.svg'
+import backgroundImg from '../assets/images/background-logo.svg'
+
+import '../styles/home.scss'
+
 export function Home() {
     return (
-        <div>
+        <div id="home-page">
             <aside>
-
+                <img src={logoIcb} alt="Logo ICB"/>
+                <strong>Igreja de Cristo em Brasília</strong>
+                <p>Uma família que ama você</p>
             </aside>
+            <main className="main-content">
+                <div>
+                    <img src={backgroundImg} alt="Igreja de Cristo"/>
+                </div>
+            </main>
         </div>
     )
 }
