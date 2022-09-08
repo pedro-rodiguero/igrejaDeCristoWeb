dc0ffd7f1b4c13e942dcbd59f12017f1c2a0ac40
pemiinem <190139323@aluno.unb.br>
2022-09-08 16:59:09 -0300

    fix uncaught error

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import React from 'react';
 import { Home } from "./pages/Home"
 
-import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-      </AuthContextProvider>
-    </BrowserRouter>  );
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+  );
 }
 
 export default App;
